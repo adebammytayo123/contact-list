006b63fbe8b9007b0cc0770a0da5c4f5373a54ac
Akere Adetola <adetolaakere1@gmail.com>
2020-07-21 14:07:22 +0100

    fixed delete contact bug

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,60 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import Contact from './Contact';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import { getContacts,delContact } from '../../actions/contactActions';
+import React, { Component, useEffect, useState } from "react";
+import Contact from "./Contact";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { getContacts, delContact } from "../../actions/contactActions";
 
-
-
-// class Contacts extends Component {
-//   componentDidMount() {
-//     this.props.getContacts();
-//   }
-//   render() {
-//     const { contacts } = this.props;
-//     console.log('allcontacts', contacts)
-//     return (
-//       <React.Fragment>
-//         <h1 className="display-4 mb-2">
-//           <span className="text-danger">Contact</span> List
-//         </h1>
-//         {contacts.map(contact => (
-//           <Contact key={contact.id} contact={contact} contacts={contacts}/>
-//         ))}
-//       </React.Fragment>
-//     );
-//   }
-// }
-
-const Contacts = (props) => {
-  const [contactList, setContactList] = useState(props.contacts)
-  useEffect(() => {
-    getContacts()
-  },[props.contacts])
-  const onDeleteClick = id => {
-    this.props.delContact(id)
-    console.log('updated contacts!!!!!', this.props.contacts)
-
+class Contacts extends Component {
+  componentDidMount() {
+    this.props.getContacts();
   }
-  return (
-    <div>
-      <h1 className="display-4 mb-2">
-        <span className="text-danger">Contact</span> List
-    </h1>
-      {contactList.map(contact => (<Contact key={contact.id} contact={contact} contacts={contactList} onDelete={onDeleteClick} id={contact.id}/>
-      ))}
-    </div>
-  )
+  render() {
+    const { contact } = this.props;
+    console.log("allcontacts", contact);
+    return (
+      <React.Fragment>
+        <h1 className="display-4 mb-2">
+          <span className="text-danger">Contact</span> List
+        </h1>
+        {contact.map((contact) => (
+          <Contact key={contact.id} contact={contact} />
+        ))}
+      </React.Fragment>
+    );
+  }
 }
+
 Contacts.propTypes = {
-  contacts: PropTypes.array.isRequired,
+  contact: PropTypes.array.isRequired,
   getContacts: PropTypes.func.isRequired
 }
 
 const mapStateToProps = (state) => ({
-  contacts: state.contact.contacts
-})
-
-
+  contact: state.contact.contacts,
+});
 
 export default connect(mapStateToProps, { getContacts })(Contacts);
